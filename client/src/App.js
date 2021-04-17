77024a671f35a4e4d119cedaddd25215b373e0cf
Nishoake <nsribava@uwaterloo.ca>
2021-04-17 00:29:19 -0400

    Completed the functional portion of the Marketplace view

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       <header>
-        Let's Go Pokémon!
+        Gym Share
       </header>
       <Marketplace/>
     </div>
--- client/src/components/Marketplace.js
+++ client/src/components/Marketplace.js
@@ -18,21 +18,41 @@ const ALL_OTHER_EQUIPMENT = gql`
 `
 
 const Marketplace = () => {
+
+  // Application State
   const [available, setAvailable] = useState([])
+  const [onHold, setOnHold] = useState([])
+  const [checkedOut, setCheckedOut] = useState([])
 
+  // Defining the useQuery Hooks
   const green = useQuery(ALL_OTHER_EQUIPMENT, { variables: { type: "available" } })
+  const yellow = useQuery(ALL_OTHER_EQUIPMENT, { variables: { type: "hold" } })
+  const red = useQuery(ALL_OTHER_EQUIPMENT, { variables: { type: "checked out" } })
 
+  // Available Equipment Hook
   useEffect(() => {
     if (green.data) {
       setAvailable(green.data.allOtherEquipment)
-      console.log(`available: ${available}`)
     }
   }, [green.data]) // eslint-disable-line
 
-  if(green.loading)  {
+  // Equipment on hold Hook
+  useEffect(() => {
+    if(yellow.data){
+      setOnHold(yellow.data.allOtherEquipment)
+    }
+  }, [yellow.data]) // eslint-disable-line
+
+  // Equipment checked out Hook
+  useEffect(() => {
+    if(red.data){
+      setCheckedOut(red.data.allOtherEquipment)
+    }
+  }, [red.data]) // eslint-disable-line
+
+  if (green.loading || yellow.loading || red.loading)  {
     return <div>loading...</div>
-  } else if (green.error) {
-      console.log(`Error = ${green.error}`)
+  } else if (green.error || yellow.error || red.error) {
       return <div>Error retrieving Book data</div>
   }
 
@@ -43,6 +63,8 @@ const Marketplace = () => {
       </h1>
 
       <TableData label="Available Equipment" equipment={available} buttonLabel="Place Hold" />
+      <TableData label="Equipment Placed on Hold" equipment={onHold} />
+      <TableData label="Equipment Checked Out" equipment={checkedOut} />
     </div>
   )
 }
