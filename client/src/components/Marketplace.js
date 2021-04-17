b7493ba4ff9ec8d094e62c2cf5a8990aa9e91e38
Nishoake <nsribava@uwaterloo.ca>
2021-04-16 20:22:59 -0400

    Resolved intial integration issue b/w client and server

--- client/src/components/Marketplace.js
+++ client/src/components/Marketplace.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_EQUIPMENT = gql`
+  query{
+    allEquipment{
+      id,
+      user_id,
+      category,
+      weight,
+      transaction_id,
+      hold_user_id
+    }
+  }
+`
 
 const Marketplace = () => {
+  const result = useQuery(ALL_EQUIPMENT)
+
+  if(result.loading)  {
+    return <div>loading...</div>
+  }
+
   return(
     <div>
-      
+      {result.data.allEquipment.map(p => p.weight).join(', ')}
+      {/* {result.data} */}
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:3006/graphql'
+  })
+})
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
   document.getElementById('root')
 )
